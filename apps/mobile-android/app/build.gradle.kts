b2fdcfc199207730536d4cfe034ed168010b71b7
viandwi24 <viandwi24@pm.me>
2023-11-22 18:50:21 +0700

    :construction_worker: ci: change compile sdk to 34

--- apps/mobile-android/app/build.gradle.kts
+++ apps/mobile-android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.nakama.capstone.linkdlaw"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.nakama.capstone.linkdlaw"
