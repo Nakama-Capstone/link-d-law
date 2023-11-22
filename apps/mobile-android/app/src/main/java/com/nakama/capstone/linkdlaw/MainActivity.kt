abc4d724951abe22c542f72a00cc3daae026f5d6
Rasid17 <rasid457@gmail.com>
2023-11-22 16:17:44 +0700

    :tada: feat: init mobile apps project

---
+++ apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.nakama.capstone.linkdlaw
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nakama.capstone.linkdlaw.ui.theme.LinkDLawTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LinkDLawTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LinkDLawTheme {
+        Greeting("Android")
+    }
+}
