287ae4a0a5f875fba7fee2b6bd9f6e0afb4f849b
ahikmatyar18 <abdullah181003@gmail.com>
2023-12-05 22:40:53 +0700

    ✨feat: add DaftarhukumScreen, DetailHukumScreen, PengacaraScreen, PesanScreen, SearchBar component

---
+++ apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/screen/auth/components/SearchBar.kt
@@ -0,0 +1,66 @@
+package com.nakama.capstone.linkdlaw.screen.auth.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nakama.capstone.linkdlaw.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SearchBar(modifier: Modifier = Modifier){
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+
+    androidx.compose.material3.SearchBar(
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = SearchBarDefaults.colors(Color(0xFFD9D9D9)),
+        query = text,
+        onQueryChange = { text = it },
+        onSearch = { active = false },
+        active = active,
+        onActiveChange = { active = it },
+        placeholder = { Text(text = "Search") },
+        leadingIcon = {
+            Icon(painterResource(id = R.drawable.ic_search), contentDescription = "Search", modifier = Modifier.size(24.dp))
+        },
+        trailingIcon = {
+            if (active){
+                Icon(
+                    modifier = Modifier.clickable {
+                        if (text.isNotEmpty()){
+                            text = ""
+                        } else {
+                            active = false
+                        }
+                    },
+                    imageVector = Icons.Default.Clear, contentDescription = "Clear Icon"
+                )
+            }
+        }
+    ) { }
+}
+
+@Preview
+@Composable
+fun SearchBarPreview(){
+    SearchBar()
+}
---
+++ apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/screen/auth/pesan/PesanScreen.kt
@@ -0,0 +1,155 @@
+package com.nakama.capstone.linkdlaw.screen.auth.pesan
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nakama.capstone.linkdlaw.R
+import com.nakama.capstone.linkdlaw.ui.theme.Poppins
+
+@Composable
+fun PesanScreen() {
+    PesanContent()
+}
+
+@Composable
+fun PesanContent(
+
+){
+    Column(
+        modifier = Modifier
+            .padding(vertical = 18.dp, horizontal = 22.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Start),
+            text = "Pesan",
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontFamily = Poppins,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF242B32),
+            )
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.brock_lesnar),
+                contentDescription = "Brock Lesnar",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(75.dp)
+                    .clip(CircleShape)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Brock Lesnar",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                        color = Color(0xFF242B32),
+                    )
+                )
+                Text(
+                    text = "Pengacara Spesialis Kriminalis Korea",
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_light)),
+                        color = Color(0xFF242B32),
+                    )
+                )
+                Text(
+                    text = "13.15",
+                    style = TextStyle(
+                        fontSize = 10.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                        color = Color(0xFF242B32),
+                    )
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.brock_lesnar),
+                contentDescription = "Brock Lesnar",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(75.dp)
+                    .clip(CircleShape)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(12.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Brock Lesnar",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                        color = Color(0xFF242B32),
+                    )
+                )
+                Text(
+                    text = "Pengacara Spesialis Kriminalis Korea",
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_light)),
+                        color = Color(0xFF242B32),
+                    )
+                )
+                Text(
+                    text = "13.15",
+                    style = TextStyle(
+                        fontSize = 10.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                        color = Color(0xFF242B32),
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Composable
+fun PesanScreenPreview(){
+    PesanScreen()
+}
