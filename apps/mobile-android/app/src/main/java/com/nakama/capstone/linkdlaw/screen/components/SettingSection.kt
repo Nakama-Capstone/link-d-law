e1466d667c6f5982c407079c8f0f8e8b967d0c59
Rasid17 <rasid457@gmail.com>
2023-12-06 15:48:50 +0700

    ✨feat: forget to import

--- apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/screen/components/SettingSection.kt
+++ apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/screen/components/SettingSection.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.dicoding.sampleui.R
+import com.nakama.capstone.linkdlaw.R
 
 @Composable
 fun SettingSection(
