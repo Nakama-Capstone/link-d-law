b78705867242c2643a8d1482310bfd2cb96b86c7
Firsta30 <91885471+Firsta30@users.noreply.github.com>
2023-12-10 03:38:57 +0700

    :wrench: add new fitur 1, fitur 2,  update main, requirements update model hukum h5, tokenizer,

--- services/tanyakim/src/fitur2.py
+++ services/tanyakim/src/fitur2.py
@@ -1,4 +1,3 @@
-# save this as app.py
 from flask import Flask
 from env import env_config
 from flask import Flask, request, jsonify
@@ -8,6 +7,7 @@ from tensorflow import keras
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 import pickle
+
 
 # CONFIG 
 # set config from env, and set default value if not exist
@@ -34,12 +34,7 @@ def predict_jenis_hukum(text, model):
     processed_input = preprocess_input_user(text)
     prediction = model.predict(processed_input)
     return 'Hukum Pidana' if prediction[0][0] > 0.3 else 'Hukum Perdata'
-
-# ROUTES
-@app.route("/v1/tanyakim/hello")
-def hello():
-  return "Hello, World!"
-
+  
 #route fitur 2
 @app.route('/v1/role/predict', methods=['POST'])
 def predict(): 
@@ -47,17 +42,3 @@ def predict():
     input_text = data['text']
     predicted_category = predict_jenis_hukum(input_text, model)
     return jsonify({'category': predicted_category})
-
-
-
-
-
-# MAIN
-if __name__ == '__main__':
-  # dont edit this line please
-  app.run(
-    debug=True,
-    use_debugger=False,
-    use_reloader=True,
-    port=PORT,
-  )
