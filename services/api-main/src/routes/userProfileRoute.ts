955402bbccb7cb897dbf2baa23e7b73d2b987285
Bayu Swari <bayuswari.01@gmail.com>
2023-12-13 20:26:53 +0700

    :sparkles: feat: add authentication for API law

--- services/api-main/src/routes/userProfileRoute.ts
+++ services/api-main/src/routes/userProfileRoute.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { getUserProfile, updateUserProfile } from '@/controllers/userController'
-import { AuthMiddleware, RequestAuthMiddleware } from '@/middleware/middleware'
+import { AuthMiddleware } from '@/middleware/middleware'
 
 const router: Router = Router()
 
 //TODO: get user profile
 router.get("/profile", AuthMiddleware, getUserProfile)
+
+//TODO: update user profile
 router.put("/profile", AuthMiddleware, updateUserProfile)
 
 export default router
