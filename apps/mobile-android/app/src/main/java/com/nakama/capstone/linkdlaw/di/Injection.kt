92809372c3612ae9769d28ac9e786010b816360b
Rasid17 <rasid457@gmail.com>
2023-12-17 13:37:33 +0700

    ✨feat: add koin injection

---
+++ apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/di/Injection.kt
@@ -0,0 +1,71 @@
+package com.nakama.capstone.linkdlaw.di
+
+import com.nakama.capstone.linkdlaw.BuildConfig
+import com.nakama.capstone.linkdlaw.preferences.SessionPreferences
+import com.nakama.capstone.linkdlaw.remote.api.ApiService
+import com.nakama.capstone.linkdlaw.repository.AuthRepository
+import com.nakama.capstone.linkdlaw.screen.auth.login.LoginViewModel
+import com.nakama.capstone.linkdlaw.screen.auth.register.RegisterViewModel
+import com.nakama.capstone.linkdlaw.screen.settings.SettingsViewModel
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Injection {
+    
+    val apiModule = module {
+        single {
+            val dataStore: SessionPreferences = get()
+            var token = ""
+            
+            runBlocking { 
+                token = dataStore.getToken()
+            }
+            
+            val loggingInterceptor = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+            val authInterceptor = Interceptor { chain ->
+                val req = chain.request()
+                val requestHeaders = req.newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .build()
+                chain.proceed(requestHeaders)
+            }
+
+            val client = OkHttpClient.Builder().addInterceptor(loggingInterceptor)
+                .addInterceptor(authInterceptor)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+                .create(ApiService::class.java)
+            
+        }
+    }
+    
+    val preferencesModule = module {
+        single { SessionPreferences(androidContext()) }
+    }
+    
+    val viewModelModule = module { 
+        viewModel { LoginViewModel(get()) }
+        viewModel { RegisterViewModel(get()) }
+        viewModel { SettingsViewModel(get()) }
+    }
+    
+    val repositoryModule = module { 
+        single { AuthRepository(get(), get()) }
+    }
+}
