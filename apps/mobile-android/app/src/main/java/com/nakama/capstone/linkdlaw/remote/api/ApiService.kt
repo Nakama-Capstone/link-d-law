9913b1b6dde28ff25c227ccdd4ddddb1e356bc26
Rasid17 <rasid457@gmail.com>
2023-12-18 21:03:19 +0700

    🔧ci: add additional api interface

--- apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/remote/api/ApiService.kt
+++ apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/remote/api/ApiService.kt
@@ -1,11 +1,16 @@
 package com.nakama.capstone.linkdlaw.remote.api
 
+import com.nakama.capstone.linkdlaw.remote.dto.GetProfileResponse
 import com.nakama.capstone.linkdlaw.remote.dto.LoginRequest
 import com.nakama.capstone.linkdlaw.remote.dto.LoginResponse
+import com.nakama.capstone.linkdlaw.remote.dto.LogoutResponse
 import com.nakama.capstone.linkdlaw.remote.dto.RegisterRequest
 import com.nakama.capstone.linkdlaw.remote.dto.RegisterResponse
+import com.nakama.capstone.linkdlaw.remote.dto.UpdateProfileRequest
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ApiService {
     
@@ -19,4 +24,29 @@ interface ApiService {
         @Body loginRequest: LoginRequest
     ):LoginResponse
     
+    @GET("/v1/auth/logout")
+    suspend fun logout():LogoutResponse
+    
+    @GET("/v1/profile")
+    suspend fun getProfile():GetProfileResponse
+    
+    @PUT("/v1/profile")
+    suspend fun updateProfile(
+        @Body updateProfileRequest: UpdateProfileRequest
+    ):GetProfileResponse
+    
+    @GET("/v1/laws")
+    suspend fun getLaws()
+    
+    @GET("/v1/laws/")
+    suspend fun getPasal()
+    
+    @GET("/v1/chats")
+    suspend fun getAllChat()
+    
+    @GET("/v1/chats/id")
+    suspend fun getAllMessage()
+    
+    @POST("/v1/chats/")
+    suspend fun sendMessage()
 }
