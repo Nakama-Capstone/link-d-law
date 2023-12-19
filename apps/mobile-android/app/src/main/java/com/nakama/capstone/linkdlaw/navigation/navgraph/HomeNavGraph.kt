1059d942b1c6a5fc5298a9d4655658f65603bdd2
Rasid17 <rasid457@gmail.com>
2023-12-20 01:43:01 +0700

    ✨feat: adjust root and home navigation graph

--- apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/navigation/navgraph/HomeNavGraph.kt
+++ apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/navigation/navgraph/HomeNavGraph.kt
@@ -1,15 +1,16 @@
 package com.nakama.capstone.linkdlaw.navigation.navgraph
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.nakama.capstone.linkdlaw.navigation.model.BottomBarScreen
-import com.nakama.capstone.linkdlaw.screen.auth.login.LoginViewModel
 import com.nakama.capstone.linkdlaw.screen.chat.ChatClassificationScreen
 import com.nakama.capstone.linkdlaw.screen.chat.ChatKimScreen
 import com.nakama.capstone.linkdlaw.screen.chat.ChatLawyerScreen
+import com.nakama.capstone.linkdlaw.screen.chat.ChatViewModel
 import com.nakama.capstone.linkdlaw.screen.daftarhukum.DaftarhukumScreen
 import com.nakama.capstone.linkdlaw.screen.detailhukum.DetailHukumScreen
 import com.nakama.capstone.linkdlaw.screen.home.HomeContent
@@ -17,30 +18,25 @@ import com.nakama.capstone.linkdlaw.screen.pengacara.PengacaraScreen
 import com.nakama.capstone.linkdlaw.screen.pengacaraprofile.PengacaraProfileScreen
 import com.nakama.capstone.linkdlaw.screen.pesan.PesanScreen
 import com.nakama.capstone.linkdlaw.screen.profile.EditProfileScreen
+import com.nakama.capstone.linkdlaw.screen.profile.EditProfileViewModel
 import com.nakama.capstone.linkdlaw.screen.settings.SettingScreen
 import com.nakama.capstone.linkdlaw.screen.settings.SettingsViewModel
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun HomeNavGraph(rootNavController: NavController ,navController: NavHostController) {
+fun HomeNavGraph(rootNavController: NavController, navController: NavHostController) {
     NavHost(
         navController = navController,
         route = Graph.HOME,
         startDestination = BottomBarScreen.Home.route
-    ){
-        composable(route = BottomBarScreen.Home.route){
-            val authViewModel: LoginViewModel = koinViewModel()
-            
+    ) {
+        composable(route = BottomBarScreen.Home.route) {
+
             HomeContent(
                 item = listOf("text1", "text2", "text3"),
                 onSearch = {},
                 onClick = {
                     navController.navigate(HomeGraph.Setting.route)
-                    
-                    //testing logout
-//                    authViewModel.logout()
-//                    rootNavController.popBackStack()
-//                    rootNavController.navigate(Graph.AUTH)
                 },
                 toChatKimScreen = {
                     navController.navigate(HomeGraph.ChatKim.route)
@@ -50,18 +46,25 @@ fun HomeNavGraph(rootNavController: NavController ,navController: NavHostControl
                 }
             )
         }
-        
-        composable(route = HomeGraph.LawClassification.route){
+
+        composable(route = HomeGraph.LawClassification.route) {
+            val chatViewModel: ChatViewModel = koinViewModel()
+            val loadingState = chatViewModel.loading.observeAsState()
+            val resultState = chatViewModel.predictResult.observeAsState()
+            
             ChatClassificationScreen(
                 navigateBack = {
                     navController.navigateUp()
-                }
+                },
+                predictResult = resultState,
+                sendMessage = chatViewModel::getPredict,
+                loadingState = loadingState
             )
         }
-        
-        composable(route = HomeGraph.Setting.route){
-            val settingsViewModel:SettingsViewModel = koinViewModel()
-            
+
+        composable(route = HomeGraph.Setting.route) {
+            val settingsViewModel: SettingsViewModel = koinViewModel()
+
             SettingScreen(
                 logoutClick = {
                     settingsViewModel.logout()
@@ -70,69 +73,88 @@ fun HomeNavGraph(rootNavController: NavController ,navController: NavHostControl
                 },
                 onProfileClick = {
                     navController.navigate(HomeGraph.EditProfile.route)
-                }
+                },
+                getProfileData = { settingsViewModel.getProfileData() },
+                profileData = settingsViewModel.profileData.observeAsState()
             )
         }
-        
-        composable(route = BottomBarScreen.Law.route){
+
+        composable(route = BottomBarScreen.Law.route) {
             DaftarhukumScreen(
                 toLawDetail = {
                     navController.navigate(HomeGraph.DetailLaw.route)
                 }
             )
         }
-        
-        composable(route = HomeGraph.DetailLaw.route){
+
+        composable(route = HomeGraph.DetailLaw.route) {
             DetailHukumScreen(
                 navigateBack = {
                     navController.navigateUp()
                 }
             )
         }
-        
-        composable(route = BottomBarScreen.Lawyer.route){
+
+        composable(route = BottomBarScreen.Lawyer.route) {
             PengacaraScreen(
                 toDetailLawyer = {
                     navController.navigate(route = HomeGraph.DetailLawyer.route)
                 }
             )
         }
-        composable(route = BottomBarScreen.Chat.route){
+        composable(route = BottomBarScreen.Chat.route) {
             PesanScreen(
                 toDetailChat = {
                     navController.navigate(route = HomeGraph.LawyerChatDetail.route)
                 }
             )
         }
-        composable(route = HomeGraph.DetailLawyer.route){
+        composable(route = HomeGraph.DetailLawyer.route) {
             PengacaraProfileScreen(
                 navigateBack = {
                     navController.navigateUp()
+                },
+                toChatScreen = {
+                    navController.navigate(HomeGraph.LawyerChatDetail.route) {
+                        navController.popBackStack()
+                        popUpTo(BottomBarScreen.Chat.route)
+                    }
                 }
             )
         }
-        composable(route = HomeGraph.LawyerChatDetail.route){
+        composable(route = HomeGraph.LawyerChatDetail.route) {
             ChatLawyerScreen(
                 navigateBack = {
                     navController.navigateUp()
                 }
             )
         }
-        composable(HomeGraph.ChatKim.route){
+        composable(HomeGraph.ChatKim.route) {
             ChatKimScreen(
                 navigateBack = {
                     navController.navigateUp()
                 }
             )
         }
-        composable(HomeGraph.EditProfile.route){
+        composable(HomeGraph.EditProfile.route) {
+            val editProfileViewModel: EditProfileViewModel = koinViewModel()
+            val profileData = editProfileViewModel.profileData.observeAsState()
+            val editResult = editProfileViewModel.editResult.observeAsState()
+
+//            val editResult = editProfileViewModel.editResult.observeAsState()
+//            
+//            if (editResult.value == true){
+//                navController.popBackStack()
+//            }
+            
             EditProfileScreen(
+                profileData = profileData,
                 navigateBack = {
                     navController.navigateUp()
                 },
-                onSaveClick = {
-                    
-                }
+                onSaveClick = editProfileViewModel::updateProfile,
+                getProfileData = { editProfileViewModel.getProfileData() },
+                editResult = editResult.value ?: false
             )
         }
     }
@@ -140,22 +162,22 @@ fun HomeNavGraph(rootNavController: NavController ,navController: NavHostControl
 
 
 sealed class HomeGraph(val route: String) {
-    object Home: HomeGraph("home")
-    
-    object Profile: HomeGraph("profile")
-    
-    object Setting: HomeGraph("setting")
-    
-    object DetailLawyer: HomeGraph("detail_lawyer")
-    
-    object DetailLaw: HomeGraph("detail_law")
-    
-    object LawyerChatDetail: HomeGraph("lawyer_chat_detail")
-    
-    object ChatKim: HomeGraph("chat_kim")
-    
-    object LawClassification: HomeGraph("classification")
-    
-    object EditProfile: HomeGraph("edit_profile")
-    
+    object Home : HomeGraph("home")
+
+    object Profile : HomeGraph("profile")
+
+    object Setting : HomeGraph("setting")
+
+    object DetailLawyer : HomeGraph("detail_lawyer")
+
+    object DetailLaw : HomeGraph("detail_law")
+
+    object LawyerChatDetail : HomeGraph("lawyer_chat_detail")
+
+    object ChatKim : HomeGraph("chat_kim")
+
+    object LawClassification : HomeGraph("classification")
+
+    object EditProfile : HomeGraph("edit_profile")
+
 }
--- apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/navigation/navgraph/RootNavGraph.kt
+++ apps/mobile-android/app/src/main/java/com/nakama/capstone/linkdlaw/navigation/navgraph/RootNavGraph.kt
@@ -1,14 +1,20 @@
 package com.nakama.capstone.linkdlaw.navigation.navgraph
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.nakama.capstone.linkdlaw.preferences.SessionPreferences
 import com.nakama.capstone.linkdlaw.screen.home.HomeScreen
+import kotlinx.coroutines.delay
 import org.koin.compose.koinInject
 
 
@@ -19,7 +25,14 @@ fun RootNavGraph(navController: NavHostController){
     val user = pref.getUserSession().collectAsState(initial = null)
     
     
-    NavHost(navController = navController, startDestination = Graph.AUTH, route = Graph.ROOT){
+    NavHost(navController = navController, startDestination = Graph.LOADING, route = Graph.ROOT){
+        
+        composable(Graph.LOADING){
+            Box(modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center){
+                CircularProgressIndicator()
+            }
+        }
         
         authNavGraph(navController)
         composable(route = Graph.HOME) {
@@ -28,12 +41,17 @@ fun RootNavGraph(navController: NavHostController){
     }
     
     LaunchedEffect(user.value?.isLogin) {
+        delay(1000)
         if (user.value?.isLogin == true){
-            navController.navigate(Graph.HOME)
+            navController.navigate(Graph.HOME){
+                popUpTo(Graph.LOADING){ inclusive = true}
+            }
             Log.d("home", "RootNavGraph: ${user.value?.token}")
         } else {
             Log.d("auth", "RootNavGraph: ${user.value?.token}")
-            navController.navigate(Graph.AUTH)
+            navController.navigate(Graph.AUTH){
+                popUpTo(Graph.LOADING){ inclusive = true}
+            }
         }
     }
 }
@@ -42,6 +60,7 @@ object Graph {
     const val ROOT = "root_graph"
     const val AUTH = "auth_graph"
     const val HOME = "home_graph"
+    const val LOADING = "loading_graph"
     const val LAW = "law_graph"
     const val LAWYER = "lawyer_graph"
     const val CHAT = "chat_graph"
