12bbfb3f5fa9e5d0ec6d46e59a09dbecec6ed13a
viandwi24 <viandwi24@pm.me>
2023-12-22 01:52:54 +0700

    ✨ feat: add image in news api

--- services/api-main/src/routes/newRoute.ts
+++ services/api-main/src/routes/newRoute.ts
@@ -1,8 +1,10 @@
-import { getNews } from '@/controllers/newController'
+import { getNewDetail, getNews } from '@/controllers/newController'
 import { Router } from 'express'
 
 const router: Router = Router()
 
 router.get('/news', getNews)
+
+router.get('/news/detail', getNewDetail)
 
 export default router
